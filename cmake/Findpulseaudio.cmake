45ecfada44826ffc08f87d5ab09dcdea93697f41
Mário Kašuba <mario.kasuba@it-academy.sk>
2015-07-31 01:52:57 +0200

    Corrected pulseaudio cmake variable name

--- cmake/Findpulseaudio.cmake
+++ cmake/Findpulseaudio.cmake
@@ -1,7 +1,7 @@
-find_path(PULSE_INCLUDE_DIR NAMES pulse/pulseaudio.h
+find_path(pulseaudio_INCLUDE_DIR NAMES pulse/pulseaudio.h
           DOC "The Pulse Audio include directory"
 )
 
-find_library(PULSE_LIBRARY NAMES pulse-simple
+find_library(pulseaudio_LIBRARY NAMES pulse-simple
           DOC "The Pulse Audio library"
 )
